7d7b0b5cb69110cd103aa1f09fc510aae77df7cf
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-02-14 18:04:46 +0300

    App accessed with the data from endpoint.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ import SignUp from './components/SignUp';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from './components/LogIn';
 import ErrorPage from './components/ErrorPage';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { EnrolleesContext } from './Contexts/EnrolleesContext';
 
 function App() {
+  const [enrollees, setEnrollees] = useState([]);
+
+  useEffect(() => {
+    const endPoint = 'http://localhost:3002/enrollees';
+    axios.get(endPoint)
+      .then(response => {
+        const data = response.data
+        setEnrollees(data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [setEnrollees]);
+
   return (
     <>
       <Router>
         <NavBar />
 
-        <Routes>
-          <Route path='/' element={<SignUp />} />
-          <Route path='/LogIn' element={<LogIn />} />
-          <Route path='*' element={<ErrorPage />} />
-        </Routes>
+        <EnrolleesContext.Provider value={{ enrollees }}>
+          <Routes>
+            <Route path='/' element={<SignUp />} />
+            <Route path='/LogIn' element={<LogIn />} />
+            <Route path='*' element={<ErrorPage />} />
+          </Routes>
+        </EnrolleesContext.Provider>
       </Router>
 
 
 
-      {/* route path='*' element={<ErrorPage />} */}    
     </>
   );
 }
