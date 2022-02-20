d4e81b332538746183e41787f702fba5b4e3b1a5
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-02-21 01:36:22 +0300

    A func. added which is callled addPerson.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import NavBar from './components/NavBar';
 import SignUp from './components/SignUp';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from './components/LogIn';
-import ErrorPage from './components/ErrorPage';
+// import ErrorPage from './components/ErrorPage';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { EnrolleesContext } from './Contexts/EnrolleesContext';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [enrollees, setEnrollees] = useState([]);
+  
+  const addPerson = (userName, pass, email) => {
+    setEnrollees([...enrollees, {id: uuidv4(), userName, pass, email}]);
+    console.log(enrollees);
+  };
 
   useEffect(() => {
     const endPoint = 'http://localhost:3002/enrollees';
@@ -17,6 +23,7 @@ function App() {
       .then(response => {
         const data = response.data
         setEnrollees(data);
+        
       })
       .catch(error => {
         console.log(error);
@@ -28,11 +35,11 @@ function App() {
       <Router>
         <NavBar />
 
-        <EnrolleesContext.Provider value={{ enrollees }}>
+        <EnrolleesContext.Provider value={{ addPerson }}>
           <Routes>
             <Route path='/' element={<SignUp />} />
             <Route path='/LogIn' element={<LogIn />} />
-            <Route path='*' element={<ErrorPage />} />
+            {/* <Route path='*' element={<ErrorPage />} /> */}
           </Routes>
         </EnrolleesContext.Provider>
       </Router>
