f5098fe9db5fc24ce20158fc1b4e163ce1c5eea6
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-02-22 00:54:27 +0300

    File have a few chages.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,25 +1,45 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import serialize from 'form-serialize';
+import { useContext } from 'react';
+import { EnrolleesContext } from '../Contexts/EnrolleesContext';
 
 function LogIn() {
-    const { userId } = useParams;
-    console.log(userId);
+    const { setLogInData, checkingDatas } = useContext(EnrolleesContext);
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        const formData = serialize(e.target, { hash: true });
+        const { formUserName, formPassword } = formData;
+        setLogInData({ formUserName, formPassword });
+    };
 
     return (
         <>
             <h2 className="position-absolute top-0 mt-4 start-50 translate-middle">Log-In</h2>
-            <form className="row g-3 mx-auto">
-                <div className="col-md-6">
-                    <label htmlFor="inputEmail4" className="form-label">Username or E-Mail</label>
-                    <input type="email" className="form-control" id="inputEmail4" />
-                </div>
-                <div className="col-md-6">
-                    <label htmlFor="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" />
-                </div>
+            <div className="container">
+                <form className="mt-5" onSubmit={handleFormSubmit}>
+                    <div className="form-row">
+                        <div className="form-group col-md-10">
+                            <label htmlFor="inputName">Username or E-Mail</label>
+                            <input type="text"
+                                className="form-control"
+                                name="formUserName" />
+                        </div>
+                        <div className="form-group col-md-2">
+                            <label htmlFor="inputRating">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                name="formPassword" />
+                        </div>
+                    </div>
+                    <Link to="/Submitted"><input type="submit" className="btn btn-danger btn-block" value="Log In" onClick={checkingDatas} /></Link>
 
-                <Link to="/"><button>Submit</button></Link>
-                {/* button  useNavigate to go log in page directly   button   */}
-            </form>
+                </form>
+
+            </div>
+            {/* button  useNavigate to go log in page directly   button   */}
+
         </>
     );
 }
