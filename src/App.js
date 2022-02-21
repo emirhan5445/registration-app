4541180116e65daa6ad74cbaa75789f72f0ed4b6
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-02-22 00:53:36 +0300

    Added a func. that checking the datas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import NavBar from './components/NavBar';
 import SignUp from './components/SignUp';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from './components/LogIn';
-// import ErrorPage from './components/ErrorPage';
+import ErrorPage from './components/ErrorPage';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { EnrolleesContext } from './Contexts/EnrolleesContext';
 import { v4 as uuidv4 } from 'uuid';
+import SubmittedPage from './components/SubmittedPage';
 
 function App() {
   const [enrollees, setEnrollees] = useState([]);
+  const [logInData, setLogInData] = useState({});
+  const { formUserName, formPassword } = logInData;
   
   const addPerson = (userName, pass, email) => {
     setEnrollees([...enrollees, {id: uuidv4(), userName, pass, email}]);
     console.log(enrollees);
+  };
+
+  const checkingDatas = () => {
+    
+    enrollees.forEach(enrollee => {
+      
+      enrollee.userName === formUserName ? console.log('true') : console.log('false');
+
+      /* console.log(enrollee.userName, enrollee.pass);
+      console.log(logInData);
+      console.log(formUserName + " / " + formPassword); */
+    })
+    
   };
 
   useEffect(() => {
@@ -23,23 +39,23 @@ function App() {
       .then(response => {
         const data = response.data
         setEnrollees(data);
-        
       })
       .catch(error => {
         console.log(error);
       })
   }, [setEnrollees]);
-
+  
   return (
     <>
       <Router>
         <NavBar />
 
-        <EnrolleesContext.Provider value={{ addPerson }}>
+        <EnrolleesContext.Provider value={{ addPerson, setLogInData, logInData, checkingDatas }}>
           <Routes>
             <Route path='/' element={<SignUp />} />
             <Route path='/LogIn' element={<LogIn />} />
-            {/* <Route path='*' element={<ErrorPage />} /> */}
+            <Route path='/Submitted' element={<SubmittedPage />} />
+            <Route path='*' element={<ErrorPage />} />
           </Routes>
         </EnrolleesContext.Provider>
       </Router>
